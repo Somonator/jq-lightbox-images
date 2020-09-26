0b8ae12d73b5d7a5316dc487bf9bb6977a1a0527
Somonator <somonator@gmail.com>
2020-09-27 00:50:14 +0300

    update loading info

--- lightbox.js
+++ lightbox.js
@@ -4,9 +4,9 @@
 			$tpl = $(`
 			<div class="lightbox" style="display: none;">
 				<div class="img-box"></div>
-				<div class="prev" style="display: none;">&larr;</div>
-				<div class="next">&rarr;</div>	
-				<div class="close">&#9747;</div>	
+				<div class="prev nav-item" style="display: none;">&larr;</div>
+				<div class="next nav-item">&rarr;</div>	
+				<div class="close nav-item">&#9747;</div>	
 			</div>
 			<div class="lightbox-overlay" style="display: none;"></div>
 			`),
@@ -25,6 +25,7 @@
 			setTemplate = function() {
 				if (!$('.lightbox').length > 0) {
 					$('body').append($tpl);
+					$tpl.filter('.lightbox');					
 				}
 
 				return $tpl;
@@ -35,7 +36,7 @@
 			getGroupImage = function(i) {
 				return $(images_group[i]);
 			},
-			checkGroup = function(image_now) {
+			checkGroup = function() {
 				if (image_now.attr('rel').match(/\[(.*?)\]/)) {
 					is_group_image = true;								
 					images_group = getImagesGroup(image_now.attr('rel'));
@@ -55,7 +56,7 @@
 				
 				image_now = $(this);
 
-				checkGroup(image_now);
+				checkGroup();
 				checkFull();				
 				setImage();
 				manageNav();
@@ -65,13 +66,23 @@
 				$('body').addClass('lightbox-overflow-hidden');
 			},				
 			setImage = function() {
-				let img = is_group_image ? getGroupImage(index_group_now) : image_now;
+				let img = is_group_image ? getGroupImage(index_group_now) : image_now, timeout_show_loading;
 
 				$lightbox.find('img').remove();
+				$lightbox.addClass('loading');
+
+				timeout_show_loading = setTimeout(function() {
+					$lightbox.append('<div class="load">loading...</div>');
+				}, 500);
+						
 				$lightbox.find('.img-box').append('<img src="' + img.attr('href') + '" alt="">');
-				$lightbox.append('<div class="load">loading...</div>');
+				$lightbox.find('img').hide();
+
 				$lightbox.find('img').on('load', function() {
-					$lightbox.find('.load').remove();
+					$lightbox.find('img').fadeIn(200);
+					$lightbox.removeClass('loading').find('.load').remove();
+
+					clearTimeout(timeout_show_loading);
 				});
 			},
 			manageNav = function() {
